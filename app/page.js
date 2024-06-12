3bec3567fb44c2c6f0df0a3e4a9b629132a7d16c
dhairya_joshi <joshidhairya79@gmail.com>
2024-06-12 16:59:54 +0530

    personal data

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { personalData } from "../utils/data/personal-data";
+import { personalData } from "./personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import { useEffect, useState } from "react";
